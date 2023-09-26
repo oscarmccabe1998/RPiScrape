213ec2eed1cde67acd17970c27869bae5b8f2667
oscarmccabe1998 <oscarmccabe98@gmail.com>
2023-09-26 22:08:40 +0100

    Updated main.py -added more comments to help follow along with the example

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ from bs4 import BeautifulSoup
 def main ():
     driver = webdriver.Firefox()
     #This example makes use of Firefox and the geckodriver 
-    driver.get('https://thepihut.com/collections/raspberry-pi-store')
+    #initial url to used to get data 
+    driver.get('https://thepihut.com/collections/raspberry-pi-store') 
     soup = BeautifulSoup(driver.page_source, 'html.parser')
+    #find HTML that contains all needed information on the products 
     Products = soup.find_all('div', attrs={'product-item__info product-item__info--with-button product-item--no-padding'})
     for item in Products:
+        #for each product found search for the name and price 
         name = item.find('a', attrs={'class':'product-item-meta__title link--hover'}).string
         price = item.find('span', attrs = {'class':'price--convertible'}).string
+        #used to get rid of random spaces in returned data 
         price = price.strip()
+        #Determin if the product is in stock depending on the type of button returned from the webpage 
         try:
             availability = item.find('span', attrs = {'class':'loader-button__text'}).string
             if availability:
