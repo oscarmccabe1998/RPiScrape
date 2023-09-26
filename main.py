6140f342fe6808fa6340d7b8a47476cd557dd2bf
oscarmccabe1998 <oscarmccabe98@gmail.com>
2023-09-26 23:21:26 +0100

    Updated main.py -refactored to an object oriented approach

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
+class ProductInfo:
+    def __init__(self, item):
+        self.item = item
+        #Search through item for name and price
+        self.name = self.item.find('a', attrs={'class':'product-item-meta__title link--hover'}).string
+        self.price = self.item.find('span', attrs = {'class':'price--convertible'}).string
+        self.price = self.price.strip()
+        self.availability = self.getProductAvailability('span', {'class':'loader-button__text'})
+        self.printRes()
+
+    #Checks if an item is available through expected error when item is not available 
+    #if the tag has text within it then it is available, otherwise it's out of stock 
+    def getProductAvailability(self, HTMLClass, Attrs):
+        try:
+            initialAttemp = self.item.find(HTMLClass, attrs=Attrs).string
+            return "Available"
+        except(AttributeError):
+            return "Out of Stock"
+    
+    def printRes(self):
+        print(self.name)
+        print(self.price)
+        print(self.availability)
+        print("")
+
 def main ():
     driver = webdriver.Firefox()
     #This example makes use of Firefox and the geckodriver 
@@ -10,22 +35,7 @@ def main ():
     #find HTML that contains all needed information on the products 
     Products = soup.find_all('div', attrs={'product-item__info product-item__info--with-button product-item--no-padding'})
     for item in Products:
-        #for each product found search for the name and price 
-        name = item.find('a', attrs={'class':'product-item-meta__title link--hover'}).string
-        price = item.find('span', attrs = {'class':'price--convertible'}).string
-        #used to get rid of random spaces in returned data 
-        price = price.strip()
-        #Determin if the product is in stock depending on the type of button returned from the webpage 
-        try:
-            availability = item.find('span', attrs = {'class':'loader-button__text'}).string
-            if availability:
-                availability = "in Stock"
-        except(AttributeError):
-            availability = "Out of Stock"
-        print(name)
-        print(price)
-        print(availability)
-        print("")
+        currentItem = ProductInfo(item=item)
     driver.quit()
 
 if __name__ == '__main__':
