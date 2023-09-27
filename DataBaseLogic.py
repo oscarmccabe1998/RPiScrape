af6aec70cb408dab69e22b09de13dc7fd96bbd81
oscarmccabe1998 <oscarmccabe98@gmail.com>
2023-09-27 20:53:53 +0100

    Updated DataBaseLogic.py -refactored the code to reduce repition of common code -added query to get all items in the database

--- DataBaseLogic.py
+++ DataBaseLogic.py
@@ -4,17 +4,13 @@ from mysql.connector import Error
 # Artile from free code camp was used to implement database connection
 # The article can be found at https://www.freecodecamp.org/news/connect-python-with-sql/
 
-class UploadToDB():
-    def __init__(self, Product):
-        self.Product = Product
+class DatabaseCred():
+    def __init__(self):
         self.host = "INSERT HOST NAME HERE"
         self.uname = "INSERT USER NAME HERE"
         self.pwd = "INSERT PASSWORD HERE"
         self.db_name = "INSERT DATABASE NAME HERE"
         self.connection = self.create_db_connection()
-        self.query = "INSERT INTO ProgSocTest (Name, Price, Availability) VALUES (%s, %s, %s);"
-        self.data = (Product.name, Product.price, Product.availability)
-        self.executeQuery()
 
     def create_db_connection(self):
         connection = None
@@ -30,6 +26,15 @@ class UploadToDB():
             print(f"Error: '{err}'")            #error handling incase connection fails
         return connection
 
+class UploadToDB():
+    def __init__(self, Product):
+        self.Product = Product
+        self.credentials = DatabaseCred()
+        self.connection = self.credentials.connection
+        self.query = "INSERT INTO ProgSocTest (Name, Price, Availability) VALUES (%s, %s, %s);"
+        self.data = (Product.name, Product.price, Product.availability)
+        self.executeQuery()
+
     def executeQuery(self):
         cursor = self.connection.cursor()
         try:
@@ -40,7 +45,28 @@ class UploadToDB():
             print(f"Error: '{err}'")
         cursor.close()
 
+class ReadFromDB():
+    def __init__(self):
+        self.credentials = DatabaseCred()
+        self.connection = self.credentials.connection
+        self.preSavedItems = self.executeQuery()
+
+    def executeQuery(self):
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("SELECT * FROM ProgSocTest")
+            getAllresult = cursor.fetchall()
+            print(getAllresult)
+            return getAllresult
+        except Err as err:
+            print(f"Error: '{err}'")
+
 def initDBLogic(Product):
     UploadToDB(Product)
+
+if __name__ == '__main__':
+    ReadFromDB()
+
+
 
 
